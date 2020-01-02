c52f13100136f6c0437426194d3a851f2cd603de
irerimwenda <bmugendi98@gmail.com>
2020-01-03 01:38:15 +0300

    fetch requests

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
     <div id="dashboard">
-        <h5>Dashboard</h5>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h5>Employees</h5>
+            </li>
+            <li v-for="employee in employees" :key="employee.id" class="collection-item">
+                <div class="chip">{{employee.dept}}</div>
+                {{employee.employee_id}}:{{employee.name}}
+
+                <router-link :to="{name: 'view-employee', params: {employee_id: employee.employee_id}}" class="secondary-content">
+                    <i class="fa fa-eye"></i>
+                </router-link>
+            </li>
+        </ul>
 
         <div class="fixed-action-btn">
             <router-link to="/new" class="btn-floating btn-large red">
@@ -11,12 +23,29 @@
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
     name: 'dashboard',
     data() {
         return {
-
+            employees: []
         }
+    },
+    created() {
+        db.collection('employees').orderBy('dept').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                // console.log(doc.data())
+                const data = {
+                    'id': doc.data,
+                    'employee_id': doc.data().employee_id,
+                    'name': doc.data().name,
+                    'dept': doc.data().dept,
+                    'position': doc.data().position
+                }
+                this.employees.push(data)
+            })
+        })
     }
 }
 </script>
