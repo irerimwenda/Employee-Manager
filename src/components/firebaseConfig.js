ce53d8d5220043267730a11874c6c30f4d92b065
irerimwenda <bmugendi98@gmail.com>
2020-01-03 18:44:59 +0300

    firebase config

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-    piKey: "AIzaSyBzeOVNSqsAQKY3srSh0yVS4yyPH9iC6cc",
-    authDomain: "laravel-a72e8.firebaseapp.com",
-    databaseURL: "https://laravel-a72e8.firebaseio.com",
-    projectId: "laravel-a72e8",
-    storageBucket: "laravel-a72e8.appspot.com",
-    messagingSenderId: "680121451865",
-    appId: "1:680121451865:web:3de602c37172bf86183848",
-    measurementId: "G-0F7DFX7YL2"
+    piKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
